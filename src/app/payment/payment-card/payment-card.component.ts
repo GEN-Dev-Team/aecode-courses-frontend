de91d4fb2628ba1f5fe712960756c2e9247bc24c
ManuelAecode <teamdev.genplus@gmail.com>
2024-10-02 13:42:29 -0500

    Create Payment Card Component

---
+++ src/app/payment/payment-card/payment-card.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { ButtonComponent } from '../../shared/components/button/button.component';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-payment-card',
+  standalone: true,
+  imports: [CommonModule,ButtonComponent],
+  templateUrl: './payment-card.component.html',
+  styleUrl: './payment-card.component.css'
+})
+export class PaymentCardComponent {
+  @Output() isClosed = new EventEmitter<boolean>();
+  selectedMethod: string = 'yape-plin'; // Inicializamos con el método por defecto
+
+  // Método para cambiar entre métodos de pago
+  showPaymentMethod(method: string) {
+    this.selectedMethod = method; // Actualiza el método de pago seleccionado
+  }
+
+  closeModal() {
+    this.isClosed.emit(false);
+  }
+}
