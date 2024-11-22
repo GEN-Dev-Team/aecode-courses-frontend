d806b4deb18c33eec9580df6cd8d01e62dc955f4
Sthepen_EA <sthepen.espinoza29@gmail.com>
2024-11-22 14:29:16 -0500

    session test implemented and session tracking validated

--- src/app/courses/course-detail/youtube-player/youtube-player.component.ts
+++ src/app/courses/course-detail/youtube-player/youtube-player.component.ts
@@ -1,6 +1,7 @@
-import { Component, inject, Input } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, Input } from '@angular/core';
 import { SafeResourceUrl } from '@angular/platform-browser';
 import { YoutubeService } from '../../../core/services/youtube.service';
+import { ClassQuestionComponent } from '../class-question/class-question.component';
 
 declare var YT: any;
 
@@ -13,7 +14,7 @@ declare global {
 @Component({
   selector: 'app-youtube-player',
   standalone: true,
-  imports: [],
+  imports: [ClassQuestionComponent],
   templateUrl: './youtube-player.component.html',
   styleUrls: ['./youtube-player.component.css'],
 })
@@ -23,10 +24,18 @@ export class YoutubePlayerComponent {
   @Input() session_id: number = -1;
 
   youtubePlayerService: YoutubeService = inject(YoutubeService);
+  cdRef: ChangeDetectorRef = inject(ChangeDetectorRef);
 
   safeUrl: SafeResourceUrl = this.youtubePlayerService.getSafeUrl('');
   videoId: string | null = null;
+  showTest: boolean = false;
 
+  ngOnInit(): void {
+    this.youtubePlayerService.fiveSecondsToFinish$.subscribe((value) => {
+      this.showTest = value;
+      this.cdRef.detectChanges(); // Forzar la detección de cambios cuando se cambie el valor de showTest
+    });
+  }
   ngAfterViewInit(): void {
     this.initializeYoutubePlayer();
   }
@@ -40,6 +49,8 @@ export class YoutubePlayerComponent {
       //   this.unit_url_video
       // );
     } else console.log('Video no encontrado.');
+
+    // Session test trigger
   }
 
   initializeYoutubePlayer() {
