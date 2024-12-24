ecafe7bd870e67eb4a2f053943e60b789ed3f344
Sthepen_EA <sthepen.espinoza29@gmail.com>
2024-12-23 19:32:54 -0500

    fixing secondary courses price

--- src/app/courses/course-landing-detail/course-invest/course-invest.component.ts
+++ src/app/courses/course-landing-detail/course-invest/course-invest.component.ts
@@ -1,9 +1,10 @@
-import { afterNextRender, Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-course-invest',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './course-invest.component.html',
   styleUrl: './course-invest.component.css',
 })
@@ -22,9 +23,11 @@ export class CourseInvestComponent {
   }
 
   ngOnInit(): void {
-    this.finalPriceAcademy =
-      this.priceAcademy - (this.priceAcademy * this.discount) / 100;
-    this.finalPriceRegular =
-      this.priceRegular - (this.priceRegular * this.discount) / 100;
+    this.finalPriceAcademy = Math.round(
+      this.priceAcademy - (this.priceAcademy * this.discount) / 100
+    );
+    this.finalPriceRegular = Math.round(
+      this.priceRegular - (this.priceRegular * this.discount) / 100
+    );
   }
 }
