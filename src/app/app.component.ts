b89f8859bb5dd1d802305b0cdff84c0183931e14
Sthepen_EA <sthepen.espinoza29@gmail.com>
2025-01-03 11:21:25 -0500

    responsive of get free content form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Inject, PLATFORM_ID } from '@angular/core';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { RouterOutlet } from '@angular/router';
+import { CourseAsideComponent } from './courses/course-aside/course-aside.component';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, RouterOutlet],
+  imports: [HeaderComponent, RouterOutlet, CourseAsideComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'aecode-courses-frontend';
+  isMobile = false;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.checkScreenSize();
+      window.addEventListener('resize', () => this.checkScreenSize());
+    }
+  }
+
+  checkScreenSize(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      this.isMobile = window.innerWidth <= 768;
+    }
+  }
 }
--- src/app/courses/platform-course-list/get-free-content-form/get-free-content-form.component.ts
+++ src/app/courses/platform-course-list/get-free-content-form/get-free-content-form.component.ts
@@ -1,11 +1,16 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { CheckGetFreeFormIconComponent } from '../../../shared/icons/check-get-free-form-icon/check-get-free-form-icon.component';
 import { ZoomInDirective } from '../../../shared/directives/animations/zoom-in.directive';
+import { SquareIconComponent } from '../../../shared/icons/square-icon/square-icon.component';
 
 @Component({
   selector: 'app-get-free-content-form',
   standalone: true,
-  imports: [CheckGetFreeFormIconComponent, ZoomInDirective],
+  imports: [
+    CheckGetFreeFormIconComponent,
+    ZoomInDirective,
+    SquareIconComponent,
+  ],
   templateUrl: './get-free-content-form.component.html',
   styleUrl: './get-free-content-form.component.css',
 })
@@ -15,6 +20,6 @@ export class GetFreeContentFormComponent {
   isProgrammingSelected: boolean = false;
 
   sendForm() {
-    this.isClosed.emit(true);
+    this.isClosed.emit(false);
   }
 }
