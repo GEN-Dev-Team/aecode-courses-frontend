1bb57093f249856b4ad73e870dee78c71a6a7d39
Sthepen_EA <sthepen.espinoza29@gmail.com>
2025-01-13 13:50:47 -0500

    secondary course leve icons and video message

--- src/app/courses/course-list/course-list.component.ts
+++ src/app/courses/course-list/course-list.component.ts
@@ -3,11 +3,12 @@ import { Router } from '@angular/router';
 import { CourseService } from '../services/course.service';
 import { CourseItemComponent } from '../course-item/course-item.component';
 import { ISecondaryCourse } from '../interface/secondary-course/Secondary-Course';
+import { LoaderComponent } from '../../shared/components/loader/loader.component';
 
 @Component({
   selector: 'app-course-list',
   standalone: true,
-  imports: [CourseItemComponent],
+  imports: [CourseItemComponent, LoaderComponent],
   templateUrl: './course-list.component.html',
   styleUrl: './course-list.component.css',
 })
