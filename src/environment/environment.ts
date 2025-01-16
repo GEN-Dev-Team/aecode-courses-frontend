d6a11efcc5731744cd646e8792603d513cae891d
Sthepen_EA <sthepen.espinoza29@gmail.com>
2025-01-16 16:11:39 -0500

    environment set api

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  // production: true,
-  // base: 'https://backend-productodigital.onrender.com',
-  production: false,
-  base: 'https://webcoursesback-26hk.onrender.com',
+  production: true,
+  base: 'https://backend-productodigital.onrender.com',
+  // production: false,
+  // base: 'https://webcoursesback-26hk.onrender.com',
 };
