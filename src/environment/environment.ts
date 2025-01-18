382081aa34b3433dbdf1a81070bb783f4f04ff57
Sthepen_EA <sthepen.espinoza29@gmail.com>
2025-01-17 23:12:00 -0500

    update

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,10 +1,6 @@
 export const environment = {
   // production: true,
   // base: 'https://backend-productodigital.onrender.com',
-<<<<<<< HEAD
-
-=======
->>>>>>> feature/payment
   production: false,
   base: 'https://webcoursesback-26hk.onrender.com',
 };
