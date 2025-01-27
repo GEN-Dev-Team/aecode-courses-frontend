e13fb8f55fd2bf166cab5dffa6688f23376e51c9
Sthepen_EA <sthepen.espinoza29@gmail.com>
2025-01-26 21:17:10 -0500

    script to generate prerendered dynamic routes

--- src/app/shared/layouts/course-overlay/course-overlay.component.ts
+++ src/app/shared/layouts/course-overlay/course-overlay.component.ts
@@ -3,11 +3,17 @@ import { CourseService } from '../../../courses/services/course.service';
 import { AddBaseUrlPipe } from '../../../core/pipes/add-base-url.pipe';
 import { SunIconComponent } from '../../icons/sun-icon/sun-icon.component';
 import { MoonIconComponent } from '../../icons/moon-icon/moon-icon.component';
+import { WsspFloatingIconComponent } from '../../icons/wssp-floating-icon/wssp-floating-icon.component';
 
 @Component({
   selector: 'app-course-overlay',
   standalone: true,
-  imports: [AddBaseUrlPipe, SunIconComponent, MoonIconComponent],
+  imports: [
+    AddBaseUrlPipe,
+    SunIconComponent,
+    MoonIconComponent,
+    WsspFloatingIconComponent,
+  ],
   templateUrl: './course-overlay.component.html',
   styleUrl: './course-overlay.component.css',
 })
