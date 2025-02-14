5bc3573fd3665aa0bc5fdec86753ca1add1a08a7
Sthepen_EA <sthepen.espinoza29@gmail.com>
2025-02-14 11:33:55 -0500

    add footer to training view

--- src/app/shared/layouts/course-overlay/course-overlay.component.ts
+++ src/app/shared/layouts/course-overlay/course-overlay.component.ts
@@ -4,6 +4,7 @@ import { SunIconComponent } from '../../icons/sun-icon/sun-icon.component';
 import { MoonIconComponent } from '../../icons/moon-icon/moon-icon.component';
 import { WsspFloatingIconComponent } from '../../icons/wssp-floating-icon/wssp-floating-icon.component';
 import { HeaderComponent } from '../../components/header/header.component';
+import { HomeFooterComponent } from '../../../home/home-footer/home-footer.component';
 
 @Component({
   selector: 'app-course-overlay',
@@ -13,6 +14,7 @@ import { HeaderComponent } from '../../components/header/header.component';
     MoonIconComponent,
     WsspFloatingIconComponent,
     HeaderComponent,
+    HomeFooterComponent,
   ],
   templateUrl: './course-overlay.component.html',
   styleUrl: './course-overlay.component.css',
