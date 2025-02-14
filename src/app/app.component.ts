849f705651495d2ae6b2ef1f073c624b65c3fd36
Sthepen_EA <sthepen.espinoza29@gmail.com>
2025-02-13 19:45:34 -0500

    update spaces in sec course detail, animation title every 5 seconds, fix bug with paginator items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component, Inject, PLATFORM_ID } from '@angular/core';
-import { HeaderComponent } from './shared/components/header/header.component';
 import { RouterOutlet } from '@angular/router';
 import { isPlatformBrowser } from '@angular/common';
+import { ContentBlockedComponent } from './shared/components/content-blocked/content-blocked.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, RouterOutlet],
+  imports: [RouterOutlet, ContentBlockedComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -14,12 +14,19 @@ export class AppComponent {
   title = 'aecode-courses-frontend';
   isMobile = false;
 
+  mobileMessage =
+    'Nuestro sitio web no se encuentra disponible en version mobile. Por favor, usa un navegador web de escritorio.';
+
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
   ngOnInit(): void {
     if (isPlatformBrowser(this.platformId)) {
       this.checkScreenSize();
       window.addEventListener('resize', () => this.checkScreenSize());
+    }
+
+    if (this.isMobile) {
+      console.log('Mobile');
     }
   }
 
