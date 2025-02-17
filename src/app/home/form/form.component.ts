4fdcae5a331ed50577090230aba8a17aac2fae70
Sthepen_EA <sthepen.espinoza29@gmail.com>
2025-02-17 13:02:02 -0500

    home form design updated and mousemove animation

--- src/app/home/form/form.component.ts
+++ src/app/home/form/form.component.ts
@@ -1,4 +1,14 @@
-import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  HostListener,
+  inject,
+  Input,
+  Output,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 import { SquareIconComponent } from '../../shared/icons/square-icon/square-icon.component';
 import {
   FormBuilder,
@@ -30,6 +40,9 @@ export class FormComponent {
   fb: FormBuilder = inject(FormBuilder);
   toastService: ToastrService = inject(ToastrService);
   gsService: GoogleSheetService = inject(GoogleSheetService);
+  renderer: Renderer2 = inject(Renderer2);
+  @ViewChild('mouseShadow') shadowElement!: ElementRef;
+  @ViewChild('formContainer') formContainer!: ElementRef;
 
   openForm = false;
 
@@ -37,6 +50,40 @@ export class FormComponent {
     full_name: new FormControl('', Validators.required),
     email: new FormControl('', [Validators.required, Validators.email]),
   });
+
+  ngAfterViewInit(): void {
+    if (this.browserService.isBrowser()) {
+      document.addEventListener('mousemove', this.onMouseMove.bind(this));
+    }
+  }
+
+  onMouseMove(event: MouseEvent) {
+    if (!this.shadowElement || !this.formContainer) return;
+
+    const formRect = this.formContainer.nativeElement.getBoundingClientRect();
+    let { clientX, clientY } = event;
+
+    const relativeX = Math.min(
+      Math.max(clientX - formRect.left, 0),
+      formRect.width
+    );
+    const relativeY = Math.min(
+      Math.max(clientY - formRect.top, 0),
+      formRect.height
+    );
+
+    const shadowSize = 150;
+    this.renderer.setStyle(
+      this.shadowElement.nativeElement,
+      'left',
+      `${relativeX - shadowSize / 2}px`
+    );
+    this.renderer.setStyle(
+      this.shadowElement.nativeElement,
+      'top',
+      `${relativeY - shadowSize / 2}px`
+    );
+  }
 
   closeModal() {
     this.openModal.emit(false);
