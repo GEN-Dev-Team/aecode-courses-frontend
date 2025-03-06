81f2c164407285320c41e1f2970297677341a519
Sthepen_EA <sthepen.espinoza29@gmail.com>
2025-03-06 12:40:43 -0500

    dark mode of course detail, theme and whatsapp button

--- src/app/courses/course-landing-detail/course-extra-information/course-extra-information.component.ts
+++ src/app/courses/course-landing-detail/course-extra-information/course-extra-information.component.ts
@@ -1,12 +1,14 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { IFAQ } from '../../interface/secondary-course/FAQ';
 import { ITool } from '../../interface/secondary-course/Tool';
 import { FapDropdownItemComponent } from './fap-dropdown-item/fap-dropdown-item.component';
+import { AsyncPipe } from '@angular/common';
+import { ThemeService } from '../../../core/services/theme.service';
 
 @Component({
   selector: 'app-course-extra-information',
   standalone: true,
-  imports: [FapDropdownItemComponent],
+  imports: [FapDropdownItemComponent, AsyncPipe],
   templateUrl: './course-extra-information.component.html',
   styleUrl: './course-extra-information.component.css',
 })
@@ -15,6 +17,8 @@ export class CourseExtraInformationComponent {
   @Input() courseBenefits: string[] = [];
   @Input() courseFaqs: IFAQ[] = [];
   @Input() toolList!: ITool[];
+
+  themeService: ThemeService = inject(ThemeService);
 
   freqQuestIdSelected!: number;
 
