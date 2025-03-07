ec3b439a5f3e2399f919100c5b0de6f8cdebcb6a
Sthepen_EA <sthepen.espinoza29@gmail.com>
2025-03-06 19:51:23 -0500

    home community v1

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,10 +5,17 @@ import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ToastrModule } from 'ngx-toastr';
 import { provideClientHydration } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
+    importProvidersFrom(
+      RouterModule.forRoot(routes, {
+        anchorScrolling: 'enabled', // ✅ Activa el desplazamiento a fragmentos (#id)
+        scrollPositionRestoration: 'enabled', // ✅ Guarda la posición del scroll
+      })
+    ),
     provideHttpClient(withFetch()),
     provideAnimations(),
     importProvidersFrom(
