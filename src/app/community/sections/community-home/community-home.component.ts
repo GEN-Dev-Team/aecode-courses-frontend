a76423db7726f2e1dd2aef7ac8cbea07dc45bccb
Sthepen_EA <sthepen.espinoza29@gmail.com>
2025-03-10 14:59:24 -0500

    functions of community sections v1

--- src/app/community/sections/community-home/community-home.component.ts
+++ src/app/community/sections/community-home/community-home.component.ts
@@ -8,4 +8,31 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
   styleUrl: './community-home.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class CommunityHomeComponent {}
+export class CommunityHomeComponent {
+  videoList: any[] = [
+    {
+      id: 1,
+      video_url: '/assets/images/community/mundo.mp4',
+    },
+    {
+      id: 2,
+      video_url: '/assets/images/community/mundo.mp4',
+    },
+    {
+      id: 3,
+      video_url: '/assets/images/community/mundo.mp4',
+    },
+    {
+      id: 4,
+      video_url: '/assets/images/community/mundo.mp4',
+    },
+    {
+      id: 5,
+      video_url: '/assets/images/community/mundo.mp4',
+    },
+    {
+      id: 6,
+      video_url: '/assets/images/community/mundo.mp4',
+    },
+  ];
+}
