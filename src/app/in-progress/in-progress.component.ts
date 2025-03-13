88b26ec9d02d06df55685e5d7c5d08c9918ba998
Sthepen_EA <sthepen.espinoza29@gmail.com>
2025-03-13 10:50:56 -0500

    update in progress view and not found view vh

--- src/app/in-progress/in-progress.component.ts
+++ src/app/in-progress/in-progress.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HomeFooterComponent } from '../home/home-footer/home-footer.component';
+import { HomeHeaderComponent } from '../home/home-header/home-header.component';
 
 @Component({
   selector: 'app-in-progress',
   standalone: true,
-  imports: [HomeFooterComponent],
+  imports: [HomeFooterComponent, HomeHeaderComponent],
   templateUrl: './in-progress.component.html',
   styleUrl: './in-progress.component.scss',
 })
