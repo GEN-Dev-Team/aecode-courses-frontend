a30c465f37f38a740088c328d255c2242d309139
Sthepen_EA <sthepen.espinoza29@gmail.com>
2025-03-13 10:50:56 -0500

    update in progress view and not found view vh

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HomeFooterComponent } from '../home/home-footer/home-footer.component';
+import { HomeHeaderComponent } from '../home/home-header/home-header.component';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [HomeFooterComponent],
+  imports: [HomeFooterComponent, HomeHeaderComponent],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss',
 })
