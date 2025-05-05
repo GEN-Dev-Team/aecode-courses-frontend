bb427063b602c49357ffe06fd93da5fc61b111e8
Sthepen_EA <sthepen.espinoza29@gmail.com>
2025-05-05 15:24:24 -0500

    update images for new courses, fix burguer bug in responsive and card buttons redirect

--- src/app/aecode-gpt/aecode-gpt.component.ts
+++ src/app/aecode-gpt/aecode-gpt.component.ts
@@ -26,52 +26,10 @@ export class AecodeGptComponent {
   @ViewChild('messagesEnd') private messagesEnd!: ElementRef;
 
   messageList: IMessage[] = [
-    {
-      userMessage: 'Hola aecodito',
-      botResponse: '¡Hola! ¿Cómo puedo ayudarte hoy?',
-    },
-    {
-      userMessage: 'Que modelo de OpenAi eres?',
-      botResponse:
-        'Soy GPT-3, la tercera versión del modelo de lenguaje generativo preentrenado de OpenAI.',
-    },
-    {
-      userMessage: 'Cuanto tiempo tardas en responder?',
-      botResponse:
-        'Como un asistente de inteligencia artificial, mis respuestas son casi instantáneas.',
-    },
-    {
-      userMessage: 'No me mientes?',
-      botResponse:
-        'Como una inteligencia artificial, no tengo la capacidad de mentir o decir la verdad porque no tengo conciencia ni sentimientos. Solo proporciono información basada en datos previamente programados.',
-    },
-    {
-      userMessage: 'Hola aecodito',
-      botResponse: '¡Hola! ¿Cómo puedo ayudarte hoy?',
-    },
-    {
-      userMessage: 'Hola aecodito',
-      botResponse: '¡Hola! ¿Cómo puedo ayudarte hoy?',
-    },
-    {
-      userMessage: 'Que modelo de OpenAi eres?',
-      botResponse:
-        'Soy GPT-3, la tercera versión del modelo de lenguaje generativo preentrenado de OpenAI.',
-    },
-    {
-      userMessage: 'Cuanto tiempo tardas en responder?',
-      botResponse:
-        'Como un asistente de inteligencia artificial, mis respuestas son casi instantáneas.',
-    },
-    {
-      userMessage: 'No me mientes?',
-      botResponse:
-        'Como una inteligencia artificial, no tengo la capacidad de mentir o decir la verdad porque no tengo conciencia ni sentimientos. Solo proporciono información basada en datos previamente programados.',
-    },
-    {
-      userMessage: 'Hola aecodito',
-      botResponse: '¡Hola! ¿Cómo puedo ayudarte hoy?',
-    },
+    // {
+    //   userMessage: 'Hola',
+    //   botResponse: '',
+    // },
   ];
   shouldScroll: boolean = true;
 
@@ -93,20 +51,31 @@ export class AecodeGptComponent {
     }
   }
 
-  callToOpenAI(prompt: string) {
+  sendMessageToChatBot(prompt: string) {
+    const messageItem: IMessage = {
+      userMessage: prompt,
+      botResponse: '',
+    };
+
+    this.messageList.push(messageItem);
+
+    // this.openAiService.sendMessageToChatBot(prompt).subscribe({
+    //   next: (token) => (messageItem.botResponse += token),
+    //   error: (err) => console.error(err),
+    //   complete: () => console.log('Respuesta completa', messageItem),
+    // });
+
     this.openAiService.sendMessageToChatBot(prompt).subscribe({
       next: (res) => {
         console.log('Response in component:', res);
         const response = res as any;
         const chatBotResponse = response.respuesta;
 
-        const messageItem: IMessage = {
-          userMessage: prompt,
-          botResponse: chatBotResponse,
-        };
-
-        this.messageList.push(messageItem);
-        this.shouldScroll = true;
+        if (chatBotResponse) {
+          this.messageList[this.messageList.length - 1].botResponse =
+            chatBotResponse;
+          this.shouldScroll = true;
+        }
       },
       error: (err) => {
         console.error('Error al enviar el mensaje:', err);
