c2f704b3fae581d00a0832d419044a3f4be1388b
Sthepen_EA <sthepen.espinoza29@gmail.com>
2025-05-29 20:33:32 -0500

    PD-83

--- src/app/user-profile/profile-main-view.component.ts
+++ src/app/user-profile/profile-main-view.component.ts
@@ -26,5 +26,5 @@ export class ProfileMainViewComponent {
   showChangePasswordError: boolean = false;
   showMessageModal: boolean = false;
   dataUpdated: boolean = false;
-  currentView: number = 2;
+  currentView: number = 0;
 }
