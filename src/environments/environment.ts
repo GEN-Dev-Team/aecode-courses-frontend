6973edd12a4ca6ae9dd210481b78160646aae5ec
Sthepen_EA <sthepen.espinoza29@gmail.com>
2025-05-31 01:22:35 -0500

    ci: cambio de env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  // base: 'https://backend-productodigital.onrender.com',
-  base: 'https://webcoursesback-alg9.onrender.com',
+  base: 'https://backend-productodigital.onrender.com',
+  // base: 'https://webcoursesback-alg9.onrender.com',
   agent_url: 'https://aecode-chat.onrender.com',
   // agent_url: 'http://127.0.0.1:8000',
   // base: 'http://localhost:8081',
