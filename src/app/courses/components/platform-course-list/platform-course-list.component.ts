3b1655d1e550c95b8915a2066c8c3f23dc0ede4d
Sthepen_EA <sthepen.espinoza29@gmail.com>
2025-06-16 19:46:30 -0500

    update course list platform

--- src/app/courses/components/platform-course-list/platform-course-list.component.ts
+++ src/app/courses/components/platform-course-list/platform-course-list.component.ts
@@ -2,16 +2,19 @@ import { Component, inject, signal } from '@angular/core';
 import { CourseItemComponent } from '../course-item/course-item.component';
 import { LoaderComponent } from '../../../shared/components/loader/loader.component';
 import { SecondaryCourseService } from '../../services/secondary-course.service';
+import { ThemeService } from '../../../core/services/theme.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-platform-course-list',
   standalone: true,
-  imports: [CourseItemComponent, LoaderComponent],
+  imports: [CourseItemComponent, LoaderComponent, AsyncPipe],
   templateUrl: './platform-course-list.component.html',
   styleUrl: './platform-course-list.component.css',
 })
 export class PlatformCourseListComponent {
   secondaryCourses: SecondaryCourseService = inject(SecondaryCourseService);
+  themeService: ThemeService = inject(ThemeService);
 
   courseList = signal<any>([]);
 
